57cc3fdbb998bbd617f76c02b16d3333e6d6e7ea
amira-medhat <amiramedhat2016@gmail.com>
2023-11-15 04:08:16 +0200

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,66 +1,71 @@
 #!/usr/bin/python3
-import time
-import math
 import sys
+import time
 
-def pollard_rho(n):
-    if n % 2 == 0:
-        return 2
-    
+def pollards_rho(n):
+    # Pollard's rho algorithm for factorization
     x = 2
     y = 2
-    d = 1 
-    # Define the function f(x) using a lambda function
+    d = 1
+
     f = lambda x: (x**2 + 1) % n
-    # Loop until a non-trivial factor is found
+
     while d == 1:
-        # Generate new values for x using the function f(x)
         x = f(x)
-	
-        # Generate new values for y using the function f(f(y))
         y = f(f(y))
-        # Calculate the greatest common divisor between |x - y| and n
-        d = math.gcd(abs(x - y), n)
-    # Return the non-trivial factor
+        d = gcd(abs(x - y), n)
+
     return d
 
-def main():
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 2:
-        # Print the correct usage of the program
-        print("Usage: python factorize.py <file>")
-        # Exit the program if the correct arguments are not provided
-        return
-    # Get the file path from the command-line argument
-    file_path = sys.argv[1]
-    # start timing
+def gcd(a, b):
+    # Euclidean algorithm for calculating the greatest common divisor
+    while b:
+        a, b = b, a % b
+    return a
+
+def factorize(n):
+    # Factorization using Pollard's rho algorithm
+    factors = []
+    
+    while n > 1:
+        # Use Pollard's rho to find a non-trivial factor
+        factor = pollards_rho(n)
+        
+        # Divide n by the factor
+        n //= factor
+        
+        factors.extend([factor, n] if n != 1 else [factor])
+
+    return factors
+
+def main(file_path):
     start_time = time.time()
+    
     try:
-        # Open the file in read mode
         with open(file_path, 'r') as file:
-	# Read all lines of the file into a lis
-            numbers = file.readlines()
-        # Iterate through each number in the list
-        for number in numbers:
-	# Convert the string number to an integer
-            num = int(number.strip())
-	# Factorize the number using Pollard's rho algorithm
-            factor = pollard_rho(num)
-            # If the factor is equal to the original number
-            if factor == num:
-                # Print that the number is prime
-                print(f"{num} is prime.")
-            else:
-	    # Print the factors of the number
-                print(f"{num}={factor}*{num // factor}")
-
-	if (time.time() - start_time > 5):
-                print("Time limit exceeded")
-                exit()
-
-    # Print an error message if the file is not found
+            for line in file:
+                # Convert the line to an integer
+                n = int(line)
+                
+                # Check time limit
+                if time.time() - start_time > 5:
+                    print("Time limit exceeded")
+                    exit()
+                
+                # Factorize the number
+                factors = factorize(n)
+                
+                # Print the factorization
+                print(f"{n}={'*'.join(map(str, factors))}")
     except FileNotFoundError:
-        print(f"File '{file_path}' not found.")
+        print(f"Error: File '{file_path}' not found.")
+    except Exception as e:
+        print(f"Error: {e}")
 
-if __name__ == '__main__':
-    main()  # Call the main function when the script is executed
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: factors <file>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    main(file_path)
